e4d00d12d3d65a850b66a796f13f18c128275305
Vladimir Lemeshko <lemeshkovladimir1992@yandex.ru>
2023-01-18 12:55:54 +0300

    initial commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,25 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
-}
+//task1
+"use strict";
 
+function solveEquation(a, b, c) {
+  let arr;
+  arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  } else if (d === 0) {
+    arr.push(-b / (2 * a));
+  }
+
+  return arr; // array
+
+};
+
+console.log(solveEquation(1, 5, 4));
+
+
+//task2
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
