069300a56509e8b3e1090790b4d40160c92dce5b
Vladimir Lemeshko <lemeshkovladimir1992@yandex.ru>
2023-01-26 21:59:31 +0300

    Changer commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,4 +1,3 @@
-//task1
 "use strict";
 
 function solveEquation(a, b, c) {
@@ -19,7 +18,36 @@ function solveEquation(a, b, c) {
 console.log(solveEquation(1, 5, 4));
 
 
-//task2
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+
+
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+
+  if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  } else {
+
+    // количество месяцев для кредита
+    let numberOfMonth = Math.trunc((Date.parse(date) - Date.now()) / (1000 * 60 * 60 * 24 * 30));
+
+    // тело кредита
+    let s = amount - contribution;
+
+    // процентная ставка
+    let percentPart = percent / 12 / 100;
+
+    // ежемесячная оплата
+    let totalAmountMonth = s * (percentPart + (percentPart / (((1 + percentPart) ** numberOfMonth) - 1)));
+
+    // общий платёж
+    totalAmount = (totalAmountMonth * numberOfMonth).toFixed(2);
+
+    return + totalAmount;
+  }
+
 }
